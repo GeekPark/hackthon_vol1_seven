e8fdb68663e6aca77616749184fa305ff319fce8
Seven Yu <dofyyu@gmail.com>
2016-11-08 20:04:18 +0800

    fix safari

--- static/player.js
+++ static/player.js
@@ -1,27 +1,29 @@
-const sock = io(),
-    btn = $('#switch');
-let myValue = true;
-btn.on('click', evt => {
-    myValue = !myValue;
-    sock.emit('switch', {
-        value: myValue ? 'on' : 'off'
+(function () {
+    const sock = io(),
+        btn = $('#switch');
+    let myValue = true;
+    btn.on('click', evt => {
+        myValue = !myValue;
+        sock.emit('switch', {
+            value: myValue ? 'on' : 'off'
+        });
     });
-});
-sock.on('connect', () => {
-    console.log('connect');
-    sock.emit('iamplayer');
-});
-sock.on('msg', data => {
-    console.log(data);
-    if (data.code === 0) {
-        btn.val('Toggle');
-        btn.attr('disabled', false);
-    } else {
-        btn.val(data.message);
+    sock.on('connect', () => {
+        console.log('connect');
+        sock.emit('iamplayer');
+    });
+    sock.on('msg', data => {
+        console.log(data);
+        if (data.code === 0) {
+            btn.val('Toggle');
+            btn.attr('disabled', false);
+        } else {
+            btn.val(data.message);
+            btn.attr('disabled', true);
+        }
+    });
+    sock.on('over', () => {
+        btn.val('SUCCESS');
         btn.attr('disabled', true);
-    }
-});
-sock.on('over', () => {
-    btn.val('SUCCESS');
-    btn.attr('disabled', true);
-});
+    });
+})();
